369d61234e0eafc7d6cc1e9e81c1973b25c8449e
Sasha777-spec <sasaborovskij69@gmail.com>
2024-11-10 08:27:33 +0300

    Add files via upload

---
+++ Hello,World.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Вывод в консоль приветствую мир
+        print("HelloWorld!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
